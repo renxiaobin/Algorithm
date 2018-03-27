cb85ed07207263eed71197ec3223096bf9a6e03b
renxiaobin <932024239@qq.com>
2018-03-28 00:41:03 +0800

    toutiao

--- src/toutiao/Solution4.java
+++ src/toutiao/Solution4.java
@@ -1,6 +1,8 @@
 package toutiao;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution4 {
@@ -17,27 +19,51 @@ public class Solution4 {
             b[i]=scanner.nextInt();
         }
 
+        if(a.length>b.length){
+            int temp[] = a;
+            a=b;
+            b=temp;
+        }
+
         System.out.println(magic(a,b));
+
     }
 
     public static int magic(int[] a, int[] b) {
         Arrays.sort(a);
         Arrays.sort(b);
+
+        List<Integer> alist = new ArrayList(Arrays.asList(a));
+        int n=a.length,m=b.length;
         int sum_a=0,sum_b=0;
         double avg_a=0,avg_b=0;
+
         for (int i = 0; i < a.length; i++) {
             sum_a+=a[i];
         }
         for (int i = 0; i < b.length; i++) {
             sum_b+=b[i];
         }
-        avg_a=sum_a/a.length;
-        avg_b=sum_b/b.length;
+        avg_a=(double)sum_a/(double)a.length;
+        avg_b=(double)sum_b/(double)b.length;
 
         int times=0;
-        if (avg_a<avg_b){
 
+        for (int i = 0; i <m-1; i++) {
+            if (b[i]>avg_a && b[i]<avg_b && !alist.contains(b[i])) {
+                times++;
+                avg_a=(double)(avg_a*alist.size()+b[i])/(double)(alist.size()+1);
+
+                avg_b=(double)(avg_b*m-b[i])/(double)(m-1);
+                m=m-1;
+            }else {
+                continue;
+            }
         }
-        return 0;
+
+        //System.out.println(avg_a);
+        //System.out.println(avg_b);
+
+        return times;
     }
 }
